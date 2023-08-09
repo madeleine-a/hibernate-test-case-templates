f700fbd700a559d6053f05dd58bcf687f2ddc625
Madeleine Andersson <andersson.madeleine@gmail.com>
2023-08-09 07:09:07 +0200

    Added tests for 5.x

---
+++ orm/hibernate-orm-5/src/test/java/entities/InstantEntity.java
@@ -0,0 +1,33 @@
+package entities;
+
+
+
+import javax.persistence.*;
+import java.time.Instant;
+
+@NamedQuery(name = "InstantEntity.findBetween", query = "SELECT i from InstantEntity i where i.dateValue between :from and :to")
+@NamedQuery(name="InstantEntity.deleteAll", query="DELETE FROM InstantEntity i")
+@Entity
+@Table(name = "INSTANT_ENTITY")
+public class InstantEntity {
+    @Column(name = "DATE_VALUE", columnDefinition = "timestamp(9)")
+    Instant dateValue;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    public InstantEntity() {}
+
+    public void setDateValue(Instant dateValue) {
+        this.dateValue = dateValue;
+    }
+
+    @Override
+    public String toString() {
+        return "InstantEntity{" +
+                "dateValue=" + dateValue +
+                ", id=" + id +
+                '}';
+    }
+}
---
+++ orm/hibernate-orm-5/src/test/java/org/hibernate/bugs/JPAUnitTestCase.java
@@ -0,0 +1,58 @@
+package org.hibernate.bugs;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+
+import entities.InstantEntity;
+import org.assertj.core.api.Assertions;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
+
+/**
+ * This template demonstrates how to develop a test case for Hibernate ORM, using the Java Persistence API.
+ */
+public class JPAUnitTestCase {
+
+	private EntityManagerFactory entityManagerFactory;
+
+	@Before
+	public void init() {
+		entityManagerFactory = Persistence.createEntityManagerFactory( "oraclePU" );
+	}
+
+	@After
+	public void destroy() {
+		entityManagerFactory.close();
+	}
+
+	// Entities are auto-discovered, so just add them anywhere on class-path
+	// Add your tests, using standard JUnit.
+	@Test
+	public void testTimestampUTC() throws Exception {
+		EntityManager entityManager = entityManagerFactory.createEntityManager();
+		entityManager.getTransaction().begin();
+
+		Instant dateTime = Instant.now().minus(70, ChronoUnit.MINUTES);
+		InstantEntity e = new InstantEntity();
+		e.setDateValue(dateTime);
+		entityManager.persist(e);
+		entityManager.flush();
+		entityManager.clear();
+
+		Query query = entityManager.createNamedQuery("InstantEntity.findBetween", InstantEntity.class);
+		query.setParameter("from", Instant.now().minus(2, ChronoUnit.HOURS));
+		query.setParameter("to", Instant.now().minus(1, ChronoUnit.HOURS));
+		List<?> resultList = query.getResultList();
+		// Should find entity with a dateValue within the time above
+		Assertions.assertThat(resultList.size()).isEqualTo(1);
+		entityManager.getTransaction().rollback();
+		entityManager.close();
+	}
+}
